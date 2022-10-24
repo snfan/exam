af0f0df6f0d9f9f6e7a930bef2a6779be27d222f
snfan <625726371@qq.com>
2022-10-24 14:08:25 +0800

    Delete ArtifactBuild.cmd

--- .github/workflows/ArtifactBuild.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-pushd "%~dp0"
-powershell Compress-7Zip "Release" -ArchiveFileName "SampleX86.zip" -Format Zip
-powershell Compress-7Zip "x64\Release" -ArchiveFileName "SampleX64.zip" -Format Zip
-:exit
-popd
-@echo on
